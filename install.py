da66f5bc5375b9a469c98a2fbbc90e4c969f696a
Jonathan Dowland <jdowland@redhat.com>
2016-08-09 16:30:01 +0100

    initial import

---
+++ install.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the MIT license. See the LICENSE file for details.
+"""
+
+from cct.module import Module
+
+class Install(Module):
+
+    def install(self):
+        pass
