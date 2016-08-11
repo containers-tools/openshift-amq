796d23667f74db3a65dc39ac056ae1ac7fe130ba
Jonathan Dowland <jdowland@redhat.com>
2016-08-11 01:08:18 +0100

    add s2i stuff

--- install.py
+++ install.py
@@ -6,9 +6,22 @@ This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details.
 """
 
+import os
+import shutil
+
 from cct.module import Module
 
 class Install(Module):
 
     def install(self):
         pass
+
+    def install_s2i(self):
+        s2i = "/usr/local/s2i"
+        src = "/tmp/cct/cct-amq-openshift/s2i"
+
+        if not os.exists(s2i):
+            os.makedirs(s2i)
+
+        for f in os.listdir(src):
+            shutil.move(os.path.join(src,f), s2i)
