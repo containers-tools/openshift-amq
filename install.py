2cbe3af27cc08e49aecd32eb169927c7b5025fe0
Jonathan Dowland <jdowland@redhat.com>
2016-08-11 03:39:19 +0100

    Add launch scripts

--- install.py
+++ install.py
@@ -13,15 +13,23 @@ from cct.module import Module
 
 class Install(Module):
 
-    def install(self):
-        pass
+    # XXX: _check_env_vars method
+    #       _ AMQ_HOME
 
-    def install_s2i(self):
-        s2i = "/usr/local/s2i"
+    def launch(self):
+        src = "/tmp/cct/cct-amq-openshift/launch"
+        dst = self.environment['AMQ_HOME']
+        for leaf in ['bin', 'conf']:
+            s = os.path.join(src,leaf)
+            for f in os.listdir(s):
+                shutil.move(os.path.join(s,f), os.path.join(dst,leaf))
+
+    def s2i(self):
         src = "/tmp/cct/cct-amq-openshift/s2i"
+        dst = "/usr/local/s2i"
 
-        if not os.path.exists(s2i):
-            os.makedirs(s2i)
+        if not os.path.exists(dst):
+            os.makedirs(dst)
 
         for f in os.listdir(src):
-            shutil.move(os.path.join(src,f), s2i)
+            shutil.move(os.path.join(src,f), dst)
