3b71077a8f66943d6ab369076c44a32f099ae227
Jonathan Dowland <jdowland@redhat.com>
2016-08-23 14:43:28 +0100

    use xml/minidom for adjusting XML file

--- run.py
+++ run.py
@@ -8,41 +8,37 @@ of the MIT license. See the LICENSE file for details.
 
 import os
 import shutil
+import xml.dom.minidom
 
 from cct.module import Module
 
-def sed(oldstr, newstr, fname):
-    """
-    Temporary implementation of something like sed(1), for use in
-    converting shell script functionality into this module.
-    """
-    with open(fname) as f:
-        contents = f.read()
-
-    if oldstr not in contents:
-        return
-
-    with open(fname,'w') as f:
-        contents = contents.replace(oldstr,newstr)
-        f.write(contents)
-
 class Run(Module):
 
-    def configure_authentication(self):
-        # XXX: belongs in __init__
+    def setup(self):
         amq_home = os.getenv("AMQ_HOME")
         # XXX: we need to modify openshift-users.properties here, temporarily, because
         # the existing/legacy shell scripts that run *afterwards* copy it over the
         # eventual location (same for activemq.xml)
         self.users_file = "{}/conf/openshift-users.properties".format(amq_home)
         self.config_file = "{}/conf/openshift-activemq.xml".format(amq_home)
+        self.config = xml.dom.minidom.parse(self.config_file)
+
+    def teardown(self):
+        with open(self.config_file, "w") as fh:
+            self.config.writexml(fh)
+
+    def configure_authentication(self):
+        e = self.config.createElement("jaasAuthenticationPlugin")
 
         if "AMQ_USER" in os.environ and "AMQ_PASSWORD" in os.environ:
-            username = os.getenv("AMQ_USER")
-            password = os.getenv("AMQ_PASSWORD")
-            sed("##### AUTHENTICATION #####", "{}={}".format(username,password), self.users_file)
-            authentication="<jaasAuthenticationPlugin configuration=\"activemq\" />"
-        else:
-            authentication="<jaasAuthenticationPlugin configuration=\"activemq-guest\" />"
+            with open(self.users_file,"a") as fh:
+                username = os.getenv("AMQ_USER")
+                password = os.getenv("AMQ_PASSWORD")
+                fh.write("{}={}\n".format(username,password))
 
-        sed("<!-- ##### AUTHENTICATION ##### -->", authentication, self.config_file)
+            e.setAttribute("configuration", "activemq")
+        else:
+            e.setAttribute("configuration", "activemq-guest")
+
+        p = self.config.getElementsByTagName("plugins")[0]
+        p.appendChild(e)
